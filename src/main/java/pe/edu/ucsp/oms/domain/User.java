45430c8a1af65d6ed7a40365c315ed2c28bb2031
Miguel Bustamante <miguel.bustamante@ucsp.edu.pe>
2013-07-02 23:29:58 -0500

    persona arreglado

--- src/main/java/pe/edu/ucsp/oms/domain/User.java
+++ src/main/java/pe/edu/ucsp/oms/domain/User.java
@@ -2,9 +2,7 @@ package pe.edu.ucsp.oms.domain;
 
 public class User extends Person implements BaseEntity<Long> {
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
 
@@ -19,55 +17,7 @@ public class User extends Person implements BaseEntity<Long> {
 	public User() {
 	}
 
-	@Override
-	public Long getId() {
-		return id;
-	}
-	
-	@Override
-	public void setId(Long id) {
-		this.id=id;	
-	}
 
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
 
 	public Double getBalance() {
 		return balance;
