ff6a172c3e4683745114839736a2f0ea06ec4325
jAguirre <jeffrey.aguirre@ucsp.edu.pe>
2013-07-05 01:03:39 -0500

    Modificando SuperAdministrador domain y dao

--- src/main/java/pe/edu/ucsp/oms/repository/SuperAdministratorDao.java
+++ src/main/java/pe/edu/ucsp/oms/repository/SuperAdministratorDao.java
@@ -1,5 +1,13 @@
 package pe.edu.ucsp.oms.repository;
 
-public class SuperAdministratorDao {
+import java.util.List;
 
+import pe.edu.ucsp.oms.domain.SuperAdministrator;
+
+public interface SuperAdministratorDao extends GenericDao<SuperAdministrator, Long> {
+	
+	SuperAdministrator findByUser(String user);
+	List<SuperAdministrator> filterByUser(String user);
+	boolean existsSuperAdministrator(String username, String password);
+	
 }
