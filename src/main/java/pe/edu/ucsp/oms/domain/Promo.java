14a4a6686d206286008256631979a8d0108859e6
jAguirre <jeffrey.aguirre@ucsp.edu.pe>
2013-07-05 03:23:46 -0500

    Dominio de las Promociones

--- src/main/java/pe/edu/ucsp/oms/domain/Promo.java
+++ src/main/java/pe/edu/ucsp/oms/domain/Promo.java
@@ -1,19 +1,45 @@
 package pe.edu.ucsp.oms.domain;
 
 public class Promo implements BaseEntity<Integer>{
-	
-	
+		
 	private static final long serialVersionUID = 1L;
-	Integer id;
+	public final static String TABLE_NAME = "Promos";
+	
+	private Integer id;
+	private String startDate;
+	private String endDate;
+	private Integer percent; 
+	
 
-	@Override
 	public Integer getId() {
 		return id;
 	}
 
-	@Override
 	public void setId(Integer id) {
-		this.id=id;
+		this.id = id;
 	}
 	
+	public String getStartDate() {
+		return startDate;
+	}
+	
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	
+	public String getEndDate() {
+		return endDate;
+	}
+	
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	
+	public Integer getPercent() {
+		return percent;
+	}
+	
+	public void setPercent(Integer percent) {
+		this.percent = percent;
+	}
 }
